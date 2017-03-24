c2289160aafe8ddd31cb51d4f250816ff3abe02c
Mike Munhall <mike.munhall@banno.com>
2017-03-24 15:50:53 -0400

    Renamed exercise 3.29 to 3.28

--- src/main/scala/com/mikemunhall/fpinscala/ch3/Tree.scala
+++ src/main/scala/com/mikemunhall/fpinscala/ch3/Tree.scala
@@ -24,9 +24,11 @@ object Tree {
     case Branch(l, r) => 1 + (depth(l) max depth(r))
   }
 
-  // Exercise 3.29
+  // Exercise 3.28
   def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
     case Leaf(v) => Leaf(f(v))
     case Branch(l, r) => Branch(map(l)(f), map(r)(f))
   }
+
+  // Exercise 3.
 }
