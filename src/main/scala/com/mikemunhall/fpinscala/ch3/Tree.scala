2e2e6bfb8384a1ac360fcde3a8d7cbc6a3da72b2
Mike Munhall <mike.munhall@banno.com>
2017-03-24 13:38:47 -0400

    Removed unnecessary parens

--- src/main/scala/com/mikemunhall/fpinscala/ch3/Tree.scala
+++ src/main/scala/com/mikemunhall/fpinscala/ch3/Tree.scala
@@ -15,6 +15,6 @@ object Tree {
   // Exercise 3.26
   def maximum(t: Tree[Int]): Int = t match {
     case Leaf(v) => v
-    case Branch(l, r) => maximum(l).max(maximum(r))
+    case Branch(l, r) => maximum(l) max maximum(r)
   }
 }
