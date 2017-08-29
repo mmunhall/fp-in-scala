f1a279385bd912600b6306a672147377007a4a45
Mike Munhall <mike.munhall@banno.com>
2017-08-29 16:22:03 -0600

    Start of Ch 11 - Monads

---
+++ src/main/scala/com/mikemunhall/fpinscala/ch11/Functor.scala
@@ -0,0 +1,39 @@
+package com.mikemunhall.fpinscala.ch11
+
+trait Functor[F[_]] {
+  def map[A, B](fa: F[A])(f: A => B): F[B]
+
+  def distribute[A, B](fab: F[(A, B)]): (F[A], F[B]) =
+    (map(fab)(_._1), map(fab)(_._2))
+}
+
+trait Mon[F[_]] {
+  def map[A, B](fa: F[A])(f: A => B): F[B]
+
+  def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
+
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+    flatMap(fa)(a => map(fb)(b => f(a, b)))
+}
+
+trait Monad[F[_]] extends Functor[F] {
+  def unit[A](a: => A): F[A]
+
+  def flatMap[A, B](ma: F[A])(f: A => F[B]): F[B]
+
+  def map[A, B](ma: F[A])(f: A => B): F[B] =
+    flatMap(ma)(a => unit(f(a)))
+
+  def map2[A, B, C](ma: F[A], mb: F[B])(f: (A, B) => C): F[C] =
+    flatMap(ma)(a => map(mb)(b => f(a, b)))
+}
+
+object Monad {
+
+  import com.mikemunhall.fpinscala.ch8.Gen
+
+  val genMonad = new Monad[Gen] {
+    def unit[A](a: => A): Gen[A] = Gen.unit(a)
+    def flatMap[A, B](ma: Gen[A])(f: A => Gen[B]): Gen[B] = ma flatMap f
+  }
+}
